0c148b93e825d6faa537f5210be7c9da9cf2ebb5
siookereu <siookereu@gmail.com>
2019-11-22 15:27:20 +0300

    Initial commit

---
+++ Assets/cannonS.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cannonS : MonoBehaviour
+{
+    public GameObject refer;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 rot = new Vector3(
+            gameObject.transform.localRotation.eulerAngles.x,
+            refer.transform.localRotation.eulerAngles.y,
+            gameObject.transform.localRotation.eulerAngles.z
+            );
+        transform.rotation = Quaternion.Euler(rot);
+    }
+}
